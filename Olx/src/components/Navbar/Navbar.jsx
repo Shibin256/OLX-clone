f791422053d4c9bdedce9c82bf53cbc5b6bc1a3e
shibin kp <shibinkp1012@gmail.com>
2025-04-19 09:58:25 +0530

    The Navbar and Footer designed

--- Olx/src/components/Navbar/Navbar.jsx
+++ Olx/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useContext } from "react";
+import "./Navbar.css";
+import OlxLogo from "../../assets/OlxLogo";
+import Search from "../../assets/Search";
+import Arrow from "../../assets/Arrow";
+import SellButton from "../../assets/SellButton";
+import SellButtonPlus from "../../assets/SellButtonPlus";
+import Heart from "../../assets/Heart"
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="headerParentDiv">
+      <div className="headerChildDiv">
+   
+        <div className="brandName">
+            <OlxLogo />
+        </div>
+   
+        <div className="placeSearch">
+          <Search />
+          <input type="text" />
+          <Arrow />
+        </div>
 
-export default Navbar
+        <div className="productSearch">
+          <div className="input">
+            <input
+              type="text"
+              placeholder="Find car,mobile phone and more..."
+            />
+          </div>
+
+          <div className="searchAction">
+            <Search color="#ffffff"></Search>
+          </div>          
+        </div>
+        
+        <div className="language">
+          <span> ENGLISH </span>
+          <Arrow />
+        </div>
+        
+        <div className="favorate">
+              <Heart />
+        </div>
+        
+        <div className="loginPage">
+         <h3>Logout</h3>
+        </div>
+        
+          <div className="sellMenu">
+          <SellButton />
+          <div className="sellMenuContent">
+            <SellButtonPlus />
+            <span>SELL</span>
+          </div>
+          </div>
+      
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- Olx/src/pages/Home/Home.jsx
+++ Olx/src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
 
 function Home() {
   return (
     <div>
-      
+        {/* <Navbar />
+        <div className='home'>       <h2>Home page</h2>        </div>
+        <Footer /> */}
     </div>
   )
 }
